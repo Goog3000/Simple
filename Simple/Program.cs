d4c2c52950a80a689f1b650257a622e7c19f89df
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-05 11:03:10 +0100

    Add project files.

---
+++ Simple/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            data = Console.ReadLine();
+            
+            int b = Convert.ToInt32(data);
+            int a = 5;
+            Console.WriteLine(b + a);
+        }
+    }
+}
