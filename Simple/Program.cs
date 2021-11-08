512d4c3b924a85035960b3b2bb416d25671f4aa6
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-08 15:42:12 +0100

    arithmetic mean

--- Simple/Program.cs
+++ Simple/Program.cs
@@ -6,17 +6,22 @@ namespace Simple
     {
         static void Main(string[] args)
         {
-            /*string data;
-            Console.WriteLine("Enter number:");
-            data = Console.ReadLine();
+            string data1;
+            Console.WriteLine("Enter first number:");
+            data1 = Console.ReadLine();
 
-            int b = Convert.ToInt32(data);
-            int a = 5;
+            double b = Convert.ToInt32(data1);
 
-            int result = b + a;
-            Console.WriteLine("Your result: " + result);*/
+            string data2;
+            Console.WriteLine("Enter second number:");
+            data2 = Console.ReadLine();
 
-            string str = "1afsdf";
+            double a = Convert.ToInt32(data2);
+
+            double result = (b + a) / 2;
+            Console.WriteLine("Your result: " + result);
+
+            /*string str = "1afsdf";
 
             int a;
             bool result = int.TryParse(str, out a);
@@ -28,7 +33,7 @@ namespace Simple
             else
             {
                 Console.WriteLine("Error");
-            }
+            }*/
 
         }
     }
