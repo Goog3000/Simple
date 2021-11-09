e42544363c55800b5fb3c5ecbae53315d280528d
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-09 15:52:52 +0100

    operators

---
+++ Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            bool c = false;
+            bool d = true;
+
+            if (a == b)
+            {
+                Console.WriteLine(c);
+            }
+            else if(a!=b) 
+            {
+                Console.WriteLine(d);
+            }
+        }
+    }
+}
