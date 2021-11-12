de428a266779e204008e7ce99b33987c3d47ee5c
Kyrylo Biletskyi <kirill4280@gmail.com>
2021-11-12 10:44:28 +0100

    switch

--- Switch/Program.cs
+++ Switch/Program.cs
@@ -11,10 +11,8 @@ namespace Switch
             switch (a)
             {
                 case 1:
-                    Console.WriteLine("You enter number 1");
-                    break;
                 case 2:
-                    Console.WriteLine("You enter number 2");
+                    Console.WriteLine("You enter number: 1 or 2");
                     break;
                 default:
                     Console.WriteLine("You enter enother number");
